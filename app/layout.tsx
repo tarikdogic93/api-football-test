188b8d743e369b27ff4cc01b6223601cba9ed606
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-11 12:56:37 +0100

    Firestore setup.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/ui/app-sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 import "./globals.css";
 
