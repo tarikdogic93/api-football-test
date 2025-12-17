5aa9d23ab71dd2e9dc085276d0befad5b6398839
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-17 17:54:38 +0100

    Improved seasons service.

--- features/seasons/services/index.ts
+++ features/seasons/services/index.ts
@@ -6,61 +6,52 @@ import type { SeasonType } from "@/features/seasons/types";
 const ONE_DAY = 24 * 60 * 60 * 1000;
 const REF = doc(db, "meta", "seasons");
 
+export async function fetchSeasonsFromAPI(): Promise<SeasonType[]> {
+  const response = await fetch(
+    "https://v3.football.api-sports.io/leagues/seasons",
+    {
+      headers: {
+        "x-apisports-key": process.env.API_FOOTBALL_KEY!,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`API error ${response.status}`);
+  }
+
+  const json = await response.json();
+  return json.response as SeasonType[];
+}
+
 export async function getSeasons(): Promise<SeasonType[]> {
   const snapshot = await getDoc(REF);
-  const cached = snapshot.exists() ? snapshot.data() : null;
+  const cached: SeasonType[] = snapshot.exists()
+    ? snapshot.data()?.seasons || []
+    : [];
+  const updatedAt: number = snapshot.exists()
+    ? snapshot.data()?.updatedAt || 0
+    : 0;
+  const now = Date.now();
 
-  const isFresh =
-    cached && cached.updatedAt && Date.now() - cached.updatedAt < ONE_DAY;
+  const isFresh = updatedAt && now - updatedAt < ONE_DAY;
+  if (isFresh) return cached;
 
-  let seasons: SeasonType[] = cached?.seasons || [];
+  const fetchedSeasons: SeasonType[] = await fetchSeasonsFromAPI();
 
-  if (isFresh) {
-    return seasons;
-  }
-
-  try {
-    const response = await fetch(
-      "https://v3.football.api-sports.io/leagues/seasons",
-      {
-        headers: {
-          "x-apisports-key": process.env.API_FOOTBALL_KEY!,
-        },
-      }
+  const hasChanged =
+    cached.length !== fetchedSeasons.length ||
+    cached.some(
+      (season: SeasonType, index: number) => season !== fetchedSeasons[index]
     );
 
-    if (!response.ok) {
-      throw new Error(`API error ${response.status}`);
-    }
-
-    const json = await response.json();
-    const fetchedSeasons: SeasonType[] = json.response;
-
-    const hasChanged =
-      seasons.length !== fetchedSeasons.length ||
-      seasons.some((season, index) => season !== fetchedSeasons[index]);
-
-    if (hasChanged) {
-      await setDoc(
-        REF,
-        {
-          seasons: fetchedSeasons,
-          updatedAt: Date.now(),
-        },
-        { merge: true }
-      );
-
-      seasons = fetchedSeasons;
-    }
-
-    return seasons;
-  } catch (error) {
-    console.error("Failed to fetch API-Football seasons", error);
-
-    if (!cached) {
-      throw new Error("No cached seasons available");
-    }
-
-    return seasons;
+  if (hasChanged) {
+    await setDoc(
+      REF,
+      { seasons: fetchedSeasons, updatedAt: now },
+      { merge: true }
+    );
   }
+
+  return fetchedSeasons;
 }
