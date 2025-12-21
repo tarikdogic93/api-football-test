dadcc1f79ddc86f19509d7eefc2d8ae297cf7fbe
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-21 23:50:54 +0100

    Added more page sizes.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const DEFAULT_PAGE_SIZE = 8;
-export const PAGE_SIZES = [5, 6, 7, 8, 9, 10];
+export const PAGE_SIZES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
