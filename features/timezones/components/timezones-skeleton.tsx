56369826211e430da73fe1c11caa82607f47c1fa
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-21 18:42:28 +0100

    Changed the length of array to be dynamic in TimezonesSkeleton component.

--- features/timezones/components/timezones-skeleton.tsx
+++ features/timezones/components/timezones-skeleton.tsx
@@ -1,10 +1,16 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default function TimezonesSkeleton() {
+type TimezonesSkeletonProps = {
+  pageSize: number;
+};
+
+export default function TimezonesSkeleton({
+  pageSize,
+}: TimezonesSkeletonProps) {
   return (
     <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-      {Array.from({ length: 8 }).map((_, i) => (
+      {Array.from({ length: pageSize }).map((_, i) => (
         <Card key={i}>
           <CardContent>
             <Skeleton className="h-6 w-full" />
