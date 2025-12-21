d21568eb0a843db55ae81e590da3122db585b222
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-21 15:28:19 +0100

    Used card component for timezones.

--- features/timezones/components/timezone.tsx
+++ features/timezones/components/timezone.tsx
@@ -1,13 +1,16 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { TimezoneType } from "@/features/timezones/types";
 
 type TimezoneProps = TimezoneType;
 
 export default function Timezone({ name }: TimezoneProps) {
   return (
-    <div className="p-4 shadow rounded">
-      <p className="font-semibold text-primary text-center truncate max-w-full">
-        {name}
-      </p>
-    </div>
+    <Card>
+      <CardContent>
+        <p className="font-semibold text-primary text-center truncate max-w-full">
+          {name}
+        </p>
+      </CardContent>
+    </Card>
   );
 }
--- features/timezones/components/timezones-skeleton.tsx
+++ features/timezones/components/timezones-skeleton.tsx
@@ -1,12 +1,15 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function TimezonesSkeleton() {
   return (
     <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
       {Array.from({ length: 8 }).map((_, i) => (
-        <div key={i} className="p-4 shadow rounded">
-          <Skeleton className="h-6 w-full" />
-        </div>
+        <Card key={i}>
+          <CardContent>
+            <Skeleton className="h-6 w-full" />
+          </CardContent>
+        </Card>
       ))}
     </section>
   );
