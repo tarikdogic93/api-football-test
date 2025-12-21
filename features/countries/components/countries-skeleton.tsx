e23850dbfe0e152ce2d957db02df80b0670802f4
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-21 15:32:26 +0100

    Used card component for countries.

--- features/countries/components/countries-skeleton.tsx
+++ features/countries/components/countries-skeleton.tsx
@@ -1,17 +1,17 @@
+import { Card, CardContent } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CountriesSkeleton() {
   return (
     <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
       {Array.from({ length: 8 }).map((_, i) => (
-        <div
-          key={i}
-          className="p-4 shadow rounded flex flex-col items-center gap-1"
-        >
-          <Skeleton className="h-10 w-16" />
-          <Skeleton className="h-6 w-20" />
-          <Skeleton className="h-5 w-10" />
-        </div>
+        <Card key={i}>
+          <CardContent className="flex flex-col items-center gap-1">
+            <Skeleton className="h-10 w-16" />
+            <Skeleton className="h-6 w-20" />
+            <Skeleton className="h-5 w-10" />
+          </CardContent>
+        </Card>
       ))}
     </section>
   );
--- features/countries/components/country.tsx
+++ features/countries/components/country.tsx
@@ -1,26 +1,29 @@
 import Image from "next/image";
 
+import { Card, CardContent } from "@/components/ui/card";
 import { CountryType } from "@/features/countries/types";
 
 type CountryProps = CountryType;
 
 export default function Country({ name, code, flag }: CountryProps) {
   return (
-    <div className="p-4 shadow rounded flex flex-col items-center gap-1">
-      <div className="relative w-16 h-10">
-        {flag ? (
-          <Image
-            src={flag}
-            alt={name}
-            fill
-            className="object-cover rounded-md"
-          />
-        ) : (
-          <div className="w-16 h-10 bg-accent rounded-md" />
-        )}
-      </div>
-      <p className="font-semibold text-primary truncate">{name}</p>
-      <p className="text-sm text-accent-foreground">{code ?? "—"}</p>
-    </div>
+    <Card>
+      <CardContent className="flex flex-col items-center gap-1">
+        <div className="relative w-16 h-10">
+          {flag ? (
+            <Image
+              src={flag}
+              alt={name}
+              fill
+              className="object-cover rounded-md"
+            />
+          ) : (
+            <div className="w-16 h-10 bg-accent rounded-md" />
+          )}
+        </div>
+        <p className="font-semibold text-primary truncate max-w-full">{name}</p>
+        <p className="text-sm text-accent-foreground">{code ?? "—"}</p>
+      </CardContent>
+    </Card>
   );
 }
