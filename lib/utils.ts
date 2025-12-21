26f84827e244516f738c9a4b7c62148aff034fd0
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-21 23:17:05 +0100

    Created a function for generating search terms.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateSearchTerms(name: string) {
+  const lower = name.toLowerCase().trim();
+  const terms: string[] = [];
+  const words = lower.split(/\s+/);
+  for (const word of words) {
+    let prefix = "";
+    for (const char of word) {
+      prefix += char;
+      terms.push(prefix);
+    }
+  }
+  return Array.from(new Set(terms));
 }
