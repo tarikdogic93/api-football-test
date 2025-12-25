b8662e232613e45bb38bd7b38a702c7a3162aa60
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-25 19:30:17 +0100

    Changed venue type.

--- features/venues/types/index.ts
+++ features/venues/types/index.ts
@@ -1,12 +1,12 @@
 export type VenueType = {
   id: number;
-  name?: string | null;
-  address?: string | null;
-  city?: string | null;
-  country?: string | null;
-  capacity?: number | null;
-  surface?: string | null;
-  image?: string | null;
+  name: string;
+  address: string | null;
+  city: string | null;
+  country: string | null;
+  capacity: number | null;
+  surface: string | null;
+  image: string | null;
 };
 
 export type VenuesSkeletonProps = {
