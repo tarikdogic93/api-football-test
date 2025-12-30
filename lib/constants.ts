fc18d1585672f5aa87bb290324475c0319a1d126
tarikdogic93 <tarikdogic93@gmail.com>
2025-12-31 00:00:08 +0100

    Added more constants.

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,3 +2,12 @@ export const ONE_DAY = 24 * 60 * 60 * 1000;
 
 export const DEFAULT_PAGE_SIZE = 8;
 export const PAGE_SIZES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const WORLD_DOCUMENT_ID = "WORLD";
+
+export const JOB_NAMES = {
+  STORE_SEASONS: "storeSeasons",
+  STORE_COUNTRIES: "storeCountries",
+  STORE_TIMEZONES: "storeTimezones",
+  STORE_VENUES: "storeVenues",
+} as const;
