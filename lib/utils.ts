b9f94fbe9c18f9b33fc06d468c82cd337cc02771
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-02 20:04:13 +0100

    Added new utility functions.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import removeAccents from "remove-accents";
@@ -6,11 +7,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function generateSafeDocumentId(name: string): string {
+  return name.trim().replace(/[^a-zA-Z0-9_-]/g, "_");
+}
+
 export function normalizeString(str?: string | null) {
   if (!str) return "";
   return removeAccents(str.toLowerCase());
 }
 
-export function generateSafeDocumentId(name: string): string {
-  return name.trim().replace(/[^a-zA-Z0-9_-]/g, "_");
+export function exactKey(value?: string | null) {
+  return crypto.createHash("sha1").update(normalizeString(value)).digest("hex");
+}
+
+export function getQueryIndexedKey(
+  redisIndexedKey: string,
+  querySignature: string
+): string {
+  return `${redisIndexedKey}:${Buffer.from(querySignature).toString("base64")}`;
 }
