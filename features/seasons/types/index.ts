1e15839fb2748b3cbf52a5ee6aa1272c64ee52f6
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-03 22:08:00 +0100

    Made extended type for seasons.

--- features/seasons/types/index.ts
+++ features/seasons/types/index.ts
@@ -2,6 +2,10 @@ export type SeasonType = {
   year: number;
 };
 
+export type ExtendedSeasonType = SeasonType & {
+  updatedAt: number;
+};
+
 export type SeasonsAPIResponse = {
   seasons: SeasonType[];
   total: number;
