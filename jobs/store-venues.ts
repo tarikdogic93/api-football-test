b848ae32f3f0d06a2ecc6e491fc69bc8638fa506
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-03 22:17:13 +0100

    Storing query info of venues in db.

--- jobs/store-venues.ts
+++ jobs/store-venues.ts
@@ -1,20 +1,21 @@
-import { writeBatch, collection, doc } from "firebase/firestore";
-
+import { writeBatch, collection, doc, getDoc } from "firebase/firestore";
 import { JOB_NAMES, VENUES_CONSTANTS } from "@/lib/constants";
 import { db } from "@/lib/firebase";
 import { addDocuments, ensureRedisConnected } from "@/lib/redis";
 import { exactKey, getQueryIndexedKey, normalizeString } from "@/lib/utils";
 import { registerJob } from "@/lib/job-registry";
-import { VenueType } from "@/features/venues/types";
+import { ExtendedVenueType, VenueType } from "@/features/venues/types";
 
 type StoreVenuesPayload = {
   venues: VenueType[];
-  timestamp: number | string;
+  timestamp: number;
   querySignature: string;
+  queryType: "city" | "country" | "search";
+  queryValue: string;
 };
 
 async function storeVenues(payload: StoreVenuesPayload): Promise<boolean> {
-  const { venues, timestamp, querySignature } = payload;
+  const { venues, timestamp, querySignature, queryType, queryValue } = payload;
 
   if (!venues || venues.length === 0) return true;
 
@@ -23,12 +24,38 @@ async function storeVenues(payload: StoreVenuesPayload): Promise<boolean> {
 
   for (const venue of venues) {
     const documentId = String(venue.id);
+    const docRef = doc(collectionRef, documentId);
 
-    batch.set(doc(collectionRef, documentId), {
+    let existingSearch: string[] = [];
+
+    if (queryType === "search") {
+      const docSnap = await getDoc(docRef);
+      existingSearch = docSnap.exists()
+        ? docSnap.data()?.queriedSearch ?? []
+        : [];
+    }
+
+    const updatedData: ExtendedVenueType = {
       ...venue,
       updatedAt: timestamp,
-      nameLower: venue.name.toLowerCase(),
-    });
+      nameLower: normalizeString(venue.name),
+    };
+
+    if (queryType === "city") {
+      updatedData.queriedCity = normalizeString(queryValue);
+    }
+
+    if (queryType === "country") {
+      updatedData.queriedCountry = normalizeString(queryValue);
+    }
+
+    if (queryType === "search") {
+      updatedData.queriedSearch = Array.from(
+        new Set([...existingSearch, normalizeString(queryValue)])
+      );
+    }
+
+    batch.set(docRef, updatedData, { merge: true });
   }
 
   await batch.commit();
