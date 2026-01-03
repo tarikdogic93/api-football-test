96e49ce80b846ba9461c49cedc6e2dbd09a38d1a
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-03 16:58:22 +0100

    Changed text of bottom loader in countries combobox.

--- components/countries-combobox.tsx
+++ components/countries-combobox.tsx
@@ -236,7 +236,7 @@ export default function CountriesCombobox({
 
             {isLoadingMore && (
               <div className="px-2 py-1.5 text-center text-sm text-muted-foreground">
-                Loading more...
+                Loading...
               </div>
             )}
           </CommandList>
