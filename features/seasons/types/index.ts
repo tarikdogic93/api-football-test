c8e00f856b8bbd90ae6abc6d6e7b27c90b64a2cd
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-04 22:34:48 +0100

    Removed extended season type.

--- features/seasons/types/index.ts
+++ features/seasons/types/index.ts
@@ -2,10 +2,6 @@ export type SeasonType = {
   year: number;
 };
 
-export type ExtendedSeasonType = SeasonType & {
-  updatedAt: number;
-};
-
 export type SeasonsAPIResponse = {
   seasons: SeasonType[];
   total: number;
