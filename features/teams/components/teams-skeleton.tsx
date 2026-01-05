409a82d7fc167974b2adf90647271687e2cd7a91
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-05 14:45:36 +0100

    Created teams skeleton component.

---
+++ features/teams/components/teams-skeleton.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+type TeamsSkeletonProps = {
+  pageSize: number;
+};
+
+export default function TeamsSkeleton({ pageSize }: TeamsSkeletonProps) {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      {Array.from({ length: 8 }).map((_, i) => (
+        <div
+          key={i}
+          className="p-4 shadow rounded flex flex-col items-center gap-1"
+        >
+          <Skeleton className="w-28 h-28 rounded-full" />
+          <Skeleton className="w-20 h-6" />
+          <Skeleton className="w-24 h-4" />
+          <Skeleton className="w-28 h-4" />
+          <Skeleton className="w-20 h-4" />
+          <Skeleton className="w-16 h-4" />
+        </div>
+      ))}
+    </div>
+  );
+}
