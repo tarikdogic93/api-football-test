e7e43213c99a13730fc1904027ab9e18677f9419
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-09 21:42:53 +0100

    Changed the default page size to 5.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 
-export const DEFAULT_PAGE_SIZE = 8;
+export const DEFAULT_PAGE_SIZE = 5;
 export const PAGE_SIZES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const WORLD_DOCUMENT_ID = "WORLD";
