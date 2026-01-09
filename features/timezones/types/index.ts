9ec1fa2c14116c4c6f4c39bc1885a98cc27d33f8
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-09 20:45:11 +0100

    Added new type for timezones.

--- features/timezones/types/index.ts
+++ features/timezones/types/index.ts
@@ -2,6 +2,12 @@ export type TimezoneType = {
   name: string;
 };
 
+export type ParsedTimezone = {
+  continent?: string;
+  region?: string;
+  city?: string;
+};
+
 export type TimezonesAPIResponse = {
   timezones: TimezoneType[];
   total: number;
