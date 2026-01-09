f916f86aeb87f52d17474cdb146c6a855e2a7a7d
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-09 21:05:37 +0100

    Implemented a timezones main component.

---
+++ features/timezones/components/timezones-main.tsx
@@ -0,0 +1,39 @@
+import { TimezoneType } from "@/features/timezones/types";
+import TimezonesSkeleton from "@/features/timezones/components/timezones-skeleton";
+import TimezonesList from "@/features/timezones/components/timezones-list";
+
+type TimezonesMainProps = {
+  timezones: TimezoneType[];
+  loading: boolean;
+  error: string;
+  currentPage: number;
+  pageSize: number;
+};
+
+export default function TimezonesMain({
+  timezones,
+  loading,
+  error,
+  currentPage,
+  pageSize,
+}: TimezonesMainProps) {
+  const offset = (currentPage - 1) * pageSize;
+
+  if (loading) {
+    return <TimezonesSkeleton pageSize={pageSize} />;
+  }
+
+  if (timezones.length === 0) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        {error ? (
+          <p className="text-destructive">{error}</p>
+        ) : (
+          <p className="text-muted-foreground">No timezones were found</p>
+        )}
+      </div>
+    );
+  }
+
+  return <TimezonesList timezones={timezones} offset={offset} />;
+}
