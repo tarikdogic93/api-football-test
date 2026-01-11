c3c66cc03cad8eeb5bd0cbb1ada230d7296ef6a5
tarikdogic93 <tarikdogic93@gmail.com>
2026-01-11 19:33:42 +0100

    Removed unnecessary styling from countries skeleton.

--- features/countries/components/countries-skeleton.tsx
+++ features/countries/components/countries-skeleton.tsx
@@ -12,7 +12,7 @@ export default function CountriesSkeleton({
       {Array.from({ length: pageSize }).map((_, index) => (
         <div key={index} className="flex items-center gap-4 px-4 py-3">
           <Skeleton className="size-8 rounded-full" />
-          <Skeleton className="w-15 h-10 rounded-md" />
+          <Skeleton className="w-15 h-10" />
           <div className="flex-1 flex flex-col gap-0.5">
             <Skeleton className="h-5.5 w-32" />
             <Skeleton className="h-4 w-14" />
